71c08287eed063cbb4a7f55ebaa65373beb999eb
Igor Beduin <beduinigor@gmail.com>
2021-03-23 01:24:23 -0300

    Read test file and output its lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!";
+    // Passing "test.asm" as argument
+    std::ifstream myfile;
+    std::string line;
+
+    myfile.open(argv[argc-1]);
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            std::cout << line << std::endl;
+        }
+    myfile.close();
+    }
+    // TODO: 
+    //  classe parser
+    //      recebe nome do arquivo
+    //          faz primeira passagem
+
+    // TODO:
+    //  lidar com o caso que arquivo não foi passado como argumento
+
+
+
+    return 0;
 }
